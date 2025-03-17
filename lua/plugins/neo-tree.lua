df8efb1e1ee1c438a4515c2e6229ed7a60623e1d
Daniel Salazar <dsnile.salazr@gmail.com>
2025-03-16 20:34:45 -0600

    change theme and show dotfiles in neotree

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -9,7 +9,14 @@ return {
 	},
 	config = function()
 		require("neo-tree").setup({
-			close_if_last_window = true, -- Close NeoTree if it's the last window
+    filesystem = {
+        filtered_items = {
+          visible = true, 
+          hide_dotfiles = false,
+          hide_gitignored = true,
+        },
+      },
+			close_if_last_window = true, 
       window={
         width = 30,
       },
--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -9,13 +9,11 @@ return {
   --   end
   -- }
 {
-    "tiagovla/tokyodark.nvim",
-    opts = {
-        -- custom options here
-    },
-    config = function(_, opts)
-        require("tokyodark").setup(opts) -- calling setup is optional
-        vim.cmd [[colorscheme tokyodark]]
-    end,
+  "folke/tokyonight.nvim",
+  lazy = false,
+  priority = 1000,
+    config = function()
+      vim.cmd[[colorscheme tokyonight-storm]]
+    end
 }
  }
